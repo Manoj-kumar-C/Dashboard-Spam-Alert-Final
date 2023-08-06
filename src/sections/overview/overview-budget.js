01ff9cdd4e6151c922d1d8ef2c732d8e6aa9524c
manoj-chandran <developermanoj2003@gmail.com>
2023-08-06 22:46:35 +0530

    phone

--- src/sections/overview/overview-budget.js
+++ src/sections/overview/overview-budget.js
@@ -21,7 +21,7 @@ export const OverviewBudget = (props) => {
               color="text.secondary"
               variant="overline"
             >
-              sms spams
+              sms Complaints
             </Typography>
             <Typography variant="h4">
               {value}
--- src/sections/overview/overview-total-customers.js
+++ src/sections/overview/overview-total-customers.js
@@ -21,7 +21,7 @@ export const OverviewTotalCustomers = (props) => {
               color="text.secondary"
               variant="overline"
             >
-              Total Customers
+              Email Complaints
             </Typography>
             <Typography variant="h4">
               {value}
